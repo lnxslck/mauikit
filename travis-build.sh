3639b13f38b5bc40cd23bfa20530a3db2416f74a
lnxslck <bqueiros@gmail.com>
2019-03-07 23:57:19 +0000

    Add qml-module from our dev repo

--- travis-build.sh
+++ travis-build.sh
@@ -11,8 +11,12 @@ wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 apt-get --yes update
 apt-get --yes dist-upgrade
 apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5solid-dev libqt5svg5-dev ninja-build qtbase5-dev qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine pdebuild
-wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
-dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+#wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+#dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+
+wget http://repo.nxos.org/development/pool/main/m/mauikit/qml-module-org-kde-mauikit_0%2Bgit20181114-1_amd64.deb
+dpkg -i qml-module-org-kde-mauikit_0+git20181114-1_amd64.deb
+
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
