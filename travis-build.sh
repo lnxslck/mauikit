7466209de9e686d6a929b153b50c2ccbe67d81cc
lnxslck <bqueiros@gmail.com>
2019-03-08 00:13:56 +0000

    Add our repository so we can fetch our packages

--- travis-build.sh
+++ travis-build.sh
@@ -7,12 +7,15 @@ apt-get --yes install wget gnupg2
 echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' | tee /etc/apt/sources.list.d/neon-stable.list
 wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 
+### Add NXOS Repository
+echo 'deb http://repo.nxos.org/development nxos main' | tee /etc/apt/sources.list.d/nxos-development.list
+
 ### Install Dependencies
 apt-get --yes update
 apt-get --yes dist-upgrade
-apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5solid-dev libqt5svg5-dev ninja-build qtbase5-dev qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine pdebuild
-wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
-dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5solid-dev libqt5svg5-dev ninja-build qtbase5-dev qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine pdebuild  qml-module-qmltermwidget
+#wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+#dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
